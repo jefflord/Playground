1d57a942d1a754df7cd41f8ee2c8578f978ad21f
Jeff Lord <jeff@jefflord.com>
2025-09-07 06:40:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using WindowsInput;
+using WindowsInput.Native;
+
+namespace RestartDisplayDriver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sim = new InputSimulator();
+            sim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.LWIN, VirtualKeyCode.CONTROL, VirtualKeyCode.SHIFT }, VirtualKeyCode.VK_B);
+        }
+    }
+}
